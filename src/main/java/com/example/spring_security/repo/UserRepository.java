526ea5817851ad81295f58694ad477ab08c2a127
Jayneel <jayneelkanungo57@gmail.com>
2026-04-20 00:02:26 +0530

    dao userdetails service loading

--- src/main/java/com/example/spring_security/repo/UserRepository.java
+++ src/main/java/com/example/spring_security/repo/UserRepository.java
@@ -1,9 +1,12 @@
 package com.example.spring_security.repo;
 
+import com.example.spring_security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository implements J{
+public interface UserRepository extends JpaRepository<User, Integer> {
 
+    User findByUsername(String username);
 
 }
--- src/main/java/com/example/spring_security/service/MyUserDetailsService.java
+++ src/main/java/com/example/spring_security/service/MyUserDetailsService.java
@@ -1,11 +1,14 @@
 package com.example.spring_security.service;
 
+import com.example.spring_security.model.User;
 import com.example.spring_security.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
@@ -15,6 +18,13 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user =userRepository.findByUsername(username);
+
+        if(user == null){
+            System.out.println("User not found!");
+            throw new UsernameNotFoundException("User 404!");
+        }
         return null;
     }
 }
